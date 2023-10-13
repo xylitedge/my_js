b57ae6d4aff121b63909a2a3ba412717e34d5bfc
Rohaan Ghule <77665337+xylitedge@users.noreply.github.com>
2023-10-13 20:12:57 +0000

    var_let_const

---
+++ 01_basic/variables.js
@@ -0,0 +1,19 @@
+const account_ID = 290896
+let account_Email = "rohan.ghule@xylitedge.com"
+var account_password = "180993"
+let account_City = "bengaluru"
+
+let account_State =
+
+
+console.log(account_ID);
+
+console.table([account_City,account_Email,account_ID, account_password,account_State]);
+
+console.log(typeof account_State);
+
+
+/*
+prefer not to use var
+because of the issue in block and functional scope
+*/
