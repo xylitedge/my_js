fb9e9133dc8317c2bf15e80d268f0f4a5728fc88
Rohaan Ghule <77665337+xylitedge@users.noreply.github.com>
2023-10-15 18:57:25 +0000

    Datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,50 @@
+"use strict"; // treat all older code as a new js new version
+//alert(8+3)// we are using nodejs not browser 
+let Name = "Rohan"
+console.log(Name)
+let sir_name =("Ghule")
+console.log(sir_name)
+
+
+//code readabilty should be high
+
+//docs for javascript
+// https://tc39.es/ecma262/
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+
+//Data types//
+
+let name ="Rohan"
+let age = "30"
+let isloggedIn = true
+
+alert(3+3);
+console.log(age)
+
+console.table([name, age,isloggedIn, Name,sir_name])
+
+// number => 2 to power 53
+// bigint = for longest number 
+// string = ""abc
+// boolean = true/false
+// null = standalone value (one of the datatype)
+// undefined = 
+let state = null
+console.log(state)
+console.log(typeof null)
+
+
+let state_1;
+console.log(state_1)
+console.log(typeof state_1)
+
+// symbols - unique 
+
+//object 
+
+
+
+
+console.log(typeof undefined)
+
