46461081d284788efde9b7f1f8d31c33604e3088
Rohaan Ghule <77665337+xylitedge@users.noreply.github.com>
2023-10-15 19:22:55 +0000

    typeof

--- 01_basic/conversionOperation.js
+++ 01_basic/conversionOperation.js
@@ -1,3 +1,25 @@
-let score = 33
-console.log(typeof (score));
+/*let score = "33abc"
+console.log(typeof (score)); //it can be print using () as well  i.e ((score));
+console.log(typeof score) // aslo it can be use as a without () and ; 
+
+
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof Number)// type of number is function
+console.log(typeof Name)
+*/
+let score = "334a"
+console.log(score)
+
+
+//type of score
+
 console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+
+
+
+
